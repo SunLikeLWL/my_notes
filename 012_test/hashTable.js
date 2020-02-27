4a2ed924e1b44ac61c1dba3eaf4c562e70083ec4
SunlikeALWW <1040344915@qq.com>
2020-02-27 11:44:27 +0800

    算法

--- 012_test/hashTable.js
+++ 012_test/hashTable.js
@@ -1,4 +1,28 @@
 function hashTable(){
     var table = [];
-    
+    var loseloseHashCode = function(key){
+        var hash = 0;
+        for(var i=0;i<key.length;i++){
+            hash+=key.charCodeAt(i);
+        }
+        return hash%37;
+    }
+    this.put = function(key,value){
+        var position = loseloseHashCode(key);
+        console.log(position+"-"+key);
+        table[position] =  value;
+    }
+    this.get = function(key){
+        return table[loseloseHashCode(key)];
+    }
+    this.remove = function(key){
+        table[loseloseHashCode(key)] = undefined;
+    }
+    this.print = function(){
+        for(var i=0;i<table.length;i++){
+            if(table[i]!==undefined){
+                console.log(i+":"+table[i]);
+            }
+        }
+    }
 }
